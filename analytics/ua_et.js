afab271e827ad57f9034f6cec198ab9572976c4e
no-kaluszka-here <48394853+no-kaluszka-here@users.noreply.github.com>
2020-12-23 16:11:00 +0100

    Update ua_et.js

--- analytics/ua_et.js
+++ analytics/ua_et.js
@@ -1,10 +1,16 @@
 
 
-$(".action_form").on("submit", function(){
-    var input=this.name;
-    var answer=input.val();
-    if (answer == "42") alert("You know the truth");
-    else alert("Try again next time");
-});
-
-console.log($(".action_form").input);
+function ajaxpost(){
+  // (A) GET FORM DATA
+  var data = new FormData();
+  data.append("text", document.getElementById("answer").value);
+ 
+  // (B) AJAX
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "0-dummy.php"+data.toString());
+  // What to do when server responds
+  xhr.onload = function(){ console.log(this.response); };
+  xhr.send(data);
+  // (C) PREVENT HTML FORM SUBMIT
+  return false;
+}
