69c1e5764ceb4211dde23932ce6a244cc35390a2
no-kaluszka-here <48394853+no-kaluszka-here@users.noreply.github.com>
2020-12-26 15:23:51 +0100

    Update ua_et.js

--- analytics/ua_et.js
+++ analytics/ua_et.js
@@ -1,16 +1,16 @@
 
 
-function ajaxpost(){
-  // (A) GET FORM DATA
-  var data = new FormData();
-  data.append("text", document.getElementById("answer").value);
+// function ajaxpost(){
+//   // (A) GET FORM DATA
+//   var data = new FormData();
+//   data.append("text", document.getElementById("answer").value);
  
-  // (B) AJAX
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "0-dummy.php"+data.toString());
-  // What to do when server responds
-  xhr.onload = function(){ console.log(this.response); };
-  xhr.send(data);
-  // (C) PREVENT HTML FORM SUBMIT
-  return false;
-}
+//   // (B) AJAX
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", "0-dummy.php"+data.toString());
+//   // What to do when server responds
+//   xhr.onload = function(){ console.log(this.response); };
+//   xhr.send(data);
+//   // (C) PREVENT HTML FORM SUBMIT
+//   return false;
+// }
