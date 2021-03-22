f9850bed93d5b21c66e8fbff496e7a9ad4901b7c
no-kaluszka-here <48394853+no-kaluszka-here@users.noreply.github.com>
2021-03-22 20:10:58 +0100

    Update ua-et.js

--- analytics/ua-et.js
+++ analytics/ua-et.js
@@ -1,5 +1,3 @@
-
-
 // function ajaxpost(){
 //   // (A) GET FORM DATA
 //   var data = new FormData();
@@ -12,5 +10,37 @@
 //   xhr.onload = function(){ console.log(this.response); };
 //   xhr.send(data);
 //   // (C) PREVENT HTML FORM SUBMIT
-//   return false;
+//   return true;
 // }
+
+// temaplte - var c = /.*onclick\s*=\s*"gtag\u0028'event'\s*,\s*'.*'\s*,\s*\u007B'event_categoty'\s*:\s*'*.'\s*\u007D\u0029;".*/;
+
+//onclick="gtag('event','action',{'event_category':'cat'});"
+
+//onclick="gtag('event','action',{event_categoty:'category'});"
+
+
+function checkClick(){
+    var clickContent = document.getElementById("click-edit").innerHTML;
+    var c = /(&nbsp|\s)*;onclick\s*=\s*"gtag\u0028'event'\s*,\s*'.*'\s*,\s*\u007B'event_category'\s*:\s*'.*'\s*\u007D\u0029;*".*/;
+    var match = clickContent.match(c);
+    
+    if (clickContent == match) {
+        console.log("that's correct!");
+        document.getElementById("check-click").innerHTML = "Correct!";
+    } 
+    
+    else 
+    document.getElementById("check-click").innerHTML = "Try again!";
+    console.log("err");
+    console.log("actual: "+clickContent);
+    console.log("test: "+ match);
+};
+
+
+document.querySelector('[contenteditable]').addEventListener('paste', function (event) {
+    event.preventDefault();
+    document.execCommand('inserttext', false, event.clipboardData.getData('text/plain'));
+  });
+
+
